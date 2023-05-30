28fa53bea0a90c2b8a359540f9d7f381e485617d
abhikadoo <abhijeetkadoo866@gmail.com>
2023-05-30 14:50:53 +0530

    bloodbank

---
+++ bloodbank/connection.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "id20053996_root";
+$password = "BBMSroot@123";
+$dbname = "id20053996_bbms";
+
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+ ?>
